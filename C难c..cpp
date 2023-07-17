9b6bad1a4473d42bafaf0e356b4de5f28e822aef
我爱季节 <131593995+YOURdabiaojiu@users.noreply.github.com>
2023-07-17 14:19:47 +0800

    NO1质数判断.cpp

--- C难c..cpp
+++ C难c..cpp
@@ -1,37 +1,18 @@
-﻿// ConsoleApplication36.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
 #include <stdio.h>
-#include<stdlib.h>
+#include<cmath>
+using namespace std;
 int main()
 {
-  //  char* p; 
-  //  int n;
-  // scanf_s("%d",&n);
-  // p=(char*)malloc(n*sizeof(char));
-
-  // for (int i = 0; i < n; i++) {
-  //     getchar();//读入并舍弃运算符
-  //     scanf_s("%c", &p[i]);
-  //     
-  //     printf("%c\n",p[i]);
-  //}
-  //
-  //  free(p);
-  //  p = NULL;
-   /* while (getchar() !='\n') {
-        putchar('a\n');
-    }*/
-  
+  //判断一个大于1的数是不是质数
+  int number;
+  cin>>number;
+  for(int i = 2; i <= number; i++){
+    if(number==i)
+      cout<<number<<"是质数";
+    else if(number % i==0){
+      cout<<number<<"不是质数";
+      break;
+    }
+  }
+  return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
